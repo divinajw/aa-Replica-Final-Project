d6c87267d65024b3e8bbce02febbdd7513d8d9d4
Divina Justine Widjaya <divinajw@gmail.com>
2019-02-02 23:31:25 +0700

    First Coomit
    Final Exam Project

---
+++ aa copycat/Assets/EndingTheGame.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingTheGame : MonoBehaviour
+{
+    private bool gameEnded = false;
+
+    public Rotator rotator;
+    public Spawner spawner;
+
+    public Animator animator;
+
+
+    public void EndGame()
+    {
+        if (gameEnded)
+            return;
+
+        else
+        {
+            rotator.enabled = false;
+            spawner.enabled = false;
+            animator.SetTrigger("EndGame");
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
